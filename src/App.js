e9d325615916d43b9f9e00b63772680a04fda10c
ShaikMurad <shaikmurad786@gmail.com>
2023-01-18 15:42:58 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+const names = ["Cherry", "Murad", "Muradcherry"];
 function App() {
   return (
     <div className="App">
@@ -10,16 +11,25 @@ function App() {
     </div>
   );
 }
-function Msg(props) {
-  const name = props.name;
+function Counter() {
+  //let like = 10;
+  const [like, setLike] = useState(0);
   return (
     <div>
-      <h1>
-        Hello, <span className="userColorchange">{name}</span>🤩😎💐
-      </h1>
-      <p>this is super Cool</p>
+      <button onClick={() => setLike(like + 1)}>👍{like}</button>
     </div>
   );
 }
+// function Msg(props) {
+//   const name = props.name;
+//   return (
+//     <div>
+//       <h1>
+//         Hello, <span className="userColorchange">{name}</span>🤩😎💐
+//       </h1>
+//       <p>this is super Cool</p>
+//     </div>
+//   );
+// }
 
 export default App;
